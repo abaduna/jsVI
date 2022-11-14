82e929919e25c32dd5753b679314e83cfb09999b
abaduna <artuelrey@gmail.com>
2022-11-14 12:35:41 -0300

    first commit

---
+++ jsVI.js
@@ -0,0 +1,12 @@
+function decirHola(usuario){
+    return "hola" + usuario + "!"
+}
+console.log(decirHola("artu"));
+
+
+const autos = ["ford","Chevrolet","toyota","tesla"]
+
+autos.forEach((auto)=>{
+    console.log(auto);
+})
+
